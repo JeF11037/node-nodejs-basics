8377d1219104735eec2444d1797d8ecdc1a70587
Lev <lev.petryakov@gmail.com>
2022-12-05 08:13:39 +0200

    feat: implement hash functions

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,17 @@
+import { dirname } from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { createHash } from 'node:crypto';
+import { readFile } from 'node:fs/promises';
+
 const calculateHash = async () => {
-    // Write your code here 
-};
+    try {
+        const path_dir = dirname(fileURLToPath(import.meta.url));
+        const path = `${path_dir}/files/fileToCalculateHashFor.txt`;
+        readFile(path, {encoding: 'utf8'})
+        .then(content => console.log(createHash('sha256').update(content).digest('hex')));
+    } catch (error) {
+        console.error(error);
+    }
+}
 
 await calculateHash();

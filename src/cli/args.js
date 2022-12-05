2729b23a710e956055c7e6277ab1eae6f24c18a7
Lev <lev.petryakov@gmail.com>
2022-12-05 08:05:43 +0200

    feat: implement cli functions

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,23 @@
+import { argv, stdout } from 'node:process';
+
 const parseArgs = () => {
-    // Write your code here 
+    const prefix = '--';
+    try {
+        console.log('\nParsing the command line arguments...\n');
+        const argv_keys = argv.filter(v => v.startsWith(prefix));
+        let first = true;
+        for (let key of argv_keys)
+        {
+            if (first)
+                first = false;
+            else
+                stdout.write(', ')
+            stdout.write(`${key} is ${argv[argv.indexOf(key)+1]}`);
+        }
+        console.log('\n\nDone !\n');
+    } catch (error) {
+        console.error(error);
+    }
 };
 
 parseArgs();
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,18 @@
+import { env, stdout } from 'node:process';
+
 const parseEnv = () => {
-    // Write your code here 
+    const prefix = 'RSS_';
+    try {
+        console.log('\nParsing the environment variables...\n');
+        const env_variables = Object.keys(env).filter(v => v.startsWith(prefix));
+        for (let value of env_variables)
+        {
+            stdout.write(`${value} = ${env_variables[value.toString()]}; `);
+        }
+        console.log('\n\nDone !\n');
+    } catch (error) {
+        console.error(error);
+    }
 };
 
 parseEnv();
